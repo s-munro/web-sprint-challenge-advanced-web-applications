7890996c3443d8b341bfdbbd28b7ec8930ba4db1
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-13 12:26:41 -0500

    bubblepage loads colors through .get on render

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
@@ -8,6 +10,22 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+
+    const getData = () => {
+    axiosWithAuth()
+    .get('/colors')
+    .then(res => {
+      console.log('data: ', res);
+      setColorList(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
+  getData();
+  }, []);
 
   return (
     <>
